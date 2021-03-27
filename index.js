2dee6f23aeaabc8fdedfef43ad68143ac7bcf463
dijinio <dijinio@gmail.com>
2021-03-27 15:45:36 +0100

    bug fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 // Routes
 const todoRouter = require("./routes/todos");
